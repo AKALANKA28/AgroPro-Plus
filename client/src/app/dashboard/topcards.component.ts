6455d26cf8cf4563996f04202f519509d377f774
Akalanka Dias <101487259+AKALANKA28@users.noreply.github.com>
2024-08-06 10:03:30 +0530

    client commit

---
+++ client/src/app/dashboard/topcards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './topcards.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class TopCards {
+
+}
---
+++ client/src/app/sidenav/nav-data.ts
@@ -0,0 +1,23 @@
+export const navbarData = [
+    {
+        routeLink: 'dashboard',
+        name: 'Dashbaoard',
+        icon: 'fas fa-th-large'
+    },
+    {
+        routeLink: 'distributors',
+        name: 'Distributors',
+        icon: 'fas fa-hand-holding-seedling'
+    },
+     {
+        routeLink: 'finance',
+        name: 'Finance',
+        icon: 'fas fa-sack-dollar'
+    },
+     {
+        routeLink: 'community',
+        name: 'Community',
+        icon: 'fas fa-users'
+    }, 
+
+]
