adff3d7984f62330574c2ef6184c7b127d0f56aa
Akalanka Dias <101487259+AKALANKA28@users.noreply.github.com>
2024-08-06 14:48:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 
 const PORT = process.env.PORT || 8000;
