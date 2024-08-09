5244cb4fa519a4f8160f9fa9bfbe1d2551d427b0
Akalanka Dias <101487259+AKALANKA28@users.noreply.github.com>
2024-08-09 09:07:25 +0530

    add fast api

--- backend/routes/ML/fertilizerDataRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const fertilizerDataController  = require('../../controllers/ML/fertilizerPredictCtrl');
-
-router.post('/add-data', fertilizerDataController.addData);
-router.get('/', fertilizerDataController.getData);
-router.put('/update-data/:id', fertilizerDataController.updateData);
-
-module.exports = router;
