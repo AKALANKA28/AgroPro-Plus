300ef4dc6b23c81e078754df6a7d8d34ed101eca
savindu-c <it22230874@my.sliit.lk>
2024-10-01 20:42:34 +0530

    merge conflicts  resolved

--- client/App.js
+++ client/App.js
@@ -1,13 +1,13 @@
 import RootNavigation from "./navigation";
 
 
-import AddBudgetPlan from "./components/finance/AddBudgetPlan";
-import BudgetPlansScreen from "./components/finance/BudgetPlansScreen";
-import BudgetPlanOverviewScreen from "./components/finance/BudgetPlanOverviewScreen";
-import EditBudgetPlanScreen from './components/finance/EditBudgetPlanScreen';
-import MarketPriceTable from './components/finance/MarketPriceTable';
-import updateExpenses from './components/finance/updateExpenses';
-import AnalysisScreen from './components/finance/AnalysisScreen'
+// import AddBudgetPlan from "./components/finance/AddBudgetPlan";
+// import BudgetPlansScreen from "./components/finance/BudgetPlansScreen";
+// import BudgetPlanOverviewScreen from "./components/finance/BudgetPlanOverviewScreen";
+// import EditBudgetPlanScreen from './components/finance/EditBudgetPlanScreen';
+// import MarketPriceTable from './components/finance/MarketPriceTable';
+// import updateExpenses from './components/finance/updateExpenses';
+// import AnalysisScreen from './components/finance/AnalysisScreen'
 
 
 
@@ -19,17 +19,6 @@ export default function App() {
   return (
     <NavigationContainer>
             <RootNavigation />
-
-      <Stack.Navigator initialRouteName="BudgetPlansScreen" >
-        <Stack.Screen name="AddBudgetPlan" component={AddBudgetPlan} options={{headerShown:true,headerTitle: '' }}/>
-        <Stack.Screen name="BudgetPlansScreen" component={BudgetPlansScreen} options={{headerShown:true,headerTitle: '' }}/>
-        <Stack.Screen name="Budget Plan Overview" component={BudgetPlanOverviewScreen} options={{headerShown:true,headerTitle: '' }}/>
-        <Stack.Screen name="EditBudgetPlanScreen" component={EditBudgetPlanScreen} options={{headerShown:true,headerTitle: '' }}/>
-        <Stack.Screen name="MarketPriceTable" component={MarketPriceTable} options={{headerShown:true,headerTitle: '' }}/>
-        <Stack.Screen name="updateExpenses" component={updateExpenses} options={{headerShown:true,headerTitle: '' }}/>
-        <Stack.Screen name="AnalysisScreen" component={AnalysisScreen} options={{headerShown:true,headerTitle: '' }}/>
-       
-      </Stack.Navigator>
     </NavigationContainer>
   );
 }
