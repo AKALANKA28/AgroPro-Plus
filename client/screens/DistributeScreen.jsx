ee427bdd45502f22d06653f14dcb586973b6ba74
AashaniSamarakoon <ashanisamarakoon36@gmail.com>
2024-10-02 17:02:53 +0530

    update

--- client/screens/DistributeScreen.jsx
+++ client/screens/DistributeScreen.jsx
@@ -2,17 +2,17 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
-import SearchPrompt from '../components/SearchPrompt';
-import MapDisplay from '../components/MapDisplay';
+import SearchPrompt from '../components/Distributors/SearchPrompt';
+import MapDisplay from '../components/Distributors/MapDisplay';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8070'; // Adjust to your backend URL
+// axios.defaults.baseURL = 'http://127.0.0.1:8070'; // Adjust to your backend URL
 
 const DistributeScreen = () => {
   const [locations, setLocations] = useState([]);
 
   const handleSearch = async (fertiName) => {
     try {
-      const { data } = await axios.get(`/Stock/available/${fertiName}`);
+      const { data } = await axios.get(`/stock/${fertiName}`);
       const mappedLocations = data
         .filter(item => item.availability === 'Available')
         .map(item => ({
@@ -27,7 +27,7 @@ const DistributeScreen = () => {
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <SearchPrompt onSearch={handleSearch} />
       {locations.length > 0 ? (
         <MapDisplay locations={locations} />
